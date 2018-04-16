b3c6d95ebae624f51e6ba28c583d451e7021d2b8
Yosa Yon <yosa.yon@gmail.com>
2018-04-16 06:57:13 +0000

    added /reports route

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,12 @@
 class ReportController < ApplicationController
- 
+ get '/reports' do
+   if logged_in?
+    @user = User.find_by_id(session[:user_id])
+    erb :'/reports/show_report'
+   else
+    redirect to '/homepage/homepage'
+   end
+  end
 
 end
 
