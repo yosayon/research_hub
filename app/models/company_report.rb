2f7b74cd776e5c13b2951accd96613364505ab6e
Yosa Yon <yosa.yon@gmail.com>
2018-04-16 06:39:50 +0000

    added belongs_to statements in companyreport class

--- app/models/company_report.rb
+++ app/models/company_report.rb
@@ -1,7 +1,7 @@
 class CompanyReport < ActiveRecord::Base
- #belongs_to :report
- #belongs_to :company
- #belongs_to :dimension
- #belongs_to :statement
- #belongs_to :score
+ belongs_to :report
+ belongs_to :company
+ belongs_to :dimension
+ belongs_to :statement
+ belongs_to :score
 end
