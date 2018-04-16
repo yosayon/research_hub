94c7ba4fd6229fb80b27597809631a269f80f05f
Yosa Yon <yosa.yon@gmail.com>
2018-04-16 05:56:48 +0000

    added user table, class and user instance methods

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+require './config/environment'
+require 'sinatra/base'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    use Rack::Flash, :sweep => true
+    set :session_secret, "theresearchhub"
+  end
+
+
+  get '/' do 
+    erb :'/homepage/homepage'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+ has_secure_password
+ 
+ def slug
+  self.username.downcase.gsub(" ","-")
+ end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+end
