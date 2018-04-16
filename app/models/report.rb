8acf1a84423b4978107887d03dd05312ccb9519b
Yosa Yon <yosa.yon@gmail.com>
2018-04-16 18:11:57 +0000

    added ar association statements into report class

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,8 @@
 class Report < ActiveRecord::Base
  belongs_to :user
  has_many :company_reports
+ has_many :companies, through: :company_reports
+ has_many :dimensions, through: :company_reports
+ has_many :statements, through: :company_reports
+ has_many :scores, through: :company_reports
 end
