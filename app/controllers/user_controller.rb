3a7a74a692394046be77a801f6af63a1dbde82c7
Yosa Yon <yosa.yon@gmail.com>
2018-04-16 18:11:28 +0000

    fixed get signup route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
  
  
  get '/signup' do
-  if !logged_in?
+  if session[:user_id]
    erb :'/users/signup'
    else
    @user = User.find_by_id(session[:user_id])
