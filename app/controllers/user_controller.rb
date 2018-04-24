dd86496e45bdd3982c295e6bff3712af8269b1f5
Yosa Yon <yosa.yon@gmail.com>
2018-04-24 20:12:47 +0000

    replaced @user with current_user, user cannot create an account if username and password is empty

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,8 @@
 class UserController < ApplicationController
  
  get '/users/:slug' do
-  @user = User.find_by_id(session[:user_id])
-  if @user
+  if logged_in?  #validation: user cannot view another user's profile page
+   @user = current_user
    erb :'users/show'
   else
    redirect to '/login'
@@ -10,23 +10,22 @@ class UserController < ApplicationController
  end
  
  get '/users' do
-  if !logged_in?
+  if !logged_in? #validation: user cannot see the signup page if they're aready logged in.
    erb :'/users/new'
   else
-   @user = current_user
-   redirect to "/users/#{@user.slug}"
+   redirect to "/users/#{current_user.slug}"
   end
  end
  
  post '/users' do
-  @user = User.find_by(:username => params[:username])
-  if @user
-   flash[:message] = "username is already taken"
+  @user = User.find_by(:username => params[:username])#validation: username and password cannot be blank
+  if current_user || params[:username].empty? || params[:password].empty?
+   flash[:message] = "username is already taken/must enter a username and a password"
    erb :'users/new'
   else
    @user = User.create(params)
    session[:user_id] = @user.id
-   redirect to "/users/#{@user.slug}"
+   redirect to "/users/#{current_user.slug}"
   end
  end
  
@@ -35,19 +34,19 @@ class UserController < ApplicationController
  end
 
  get '/login' do
-  if !logged_in?
+  if !logged_in? #validation: user cannot view the login page if they're already logged in
    erb :'/users/login'
   else
-   @user = current_user
-   redirect to "/users/#{@user.slug}"
+   #@user = current_user
+   redirect to "/users/#{current_user.slug}"
   end
  end
  
- post '/login' do
+ post '/login' do #validation: user must enter in correct username and password to log in
   @user = User.find_by(:username => params[:username])
   if @user && @user.authenticate(params[:password])
    session[:user_id] = @user.id
-   redirect to "/users/#{@user.slug}"
+   redirect to "/users/#{current_user.slug}"
   else
    flash[:message] = "Incorrect username or password, please try again"
    erb :'users/login'
