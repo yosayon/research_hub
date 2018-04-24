41443d0a29a287d21e10a6983d1081936074cb37
Yosa Yon <yosa.yon@gmail.com>
2018-04-24 05:54:43 +0000

    Refactoring: updated report.erb to index.erb

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,7 +3,7 @@ class ReportController < ApplicationController
  get '/reports' do
   if logged_in?
    @user = User.find_by_id(session[:user_id])
-   erb :'/reports/reports'
+   erb :'/reports/index'
   else
    redirect to '/login'
   end
